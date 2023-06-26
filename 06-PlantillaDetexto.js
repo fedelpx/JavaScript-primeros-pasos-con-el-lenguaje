5c8bd5421bb9e829c2d8afbb6ca7763bdab78c9f
Federico Carranza Zavalia <fedecarranzanz@gmail.com>
2023-06-26 13:32:03 -0300

    Condiciones y flujos

--- 06-PlantillaDetexto.js
+++
@@ -1,8 +0,0 @@
-const nombrePasajero = "Leonardo";
-const apellidoPasajero = "Lacruz";
-
-let nombreCompleto = `El nombre completo es: ${nombrePasajero} ${apellidoPasajero}`;
-console.log(nombreCompleto);
-
-let segundoPasajero = "Diego Castillo"
-console.log(`El nombre del segundo pasajero es: ${segundoPasajero}`)
