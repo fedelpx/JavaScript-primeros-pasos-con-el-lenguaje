00592b428863a2c13224fadcd5f06b25431973dc
Federico Carranza Zavalia <fedecarranzanz@gmail.com>
2023-06-26 12:54:02 -0300

    Bases

---
+++ 06-PlantillaDetexto.js
@@ -0,0 +1,8 @@
+const nombrePasajero = "Leonardo";
+const apellidoPasajero = "Lacruz";
+
+let nombreCompleto = `El nombre completo es: ${nombrePasajero} ${apellidoPasajero}`;
+console.log(nombreCompleto);
+
+let segundoPasajero = "Diego Castillo"
+console.log(`El nombre del segundo pasajero es: ${segundoPasajero}`)
